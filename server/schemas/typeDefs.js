d0d8a36f4cbe04c2c8411ec4e85f92d16f95fe3e
Rogers Ramirez <rogers.ramirez2008@gmail.com>
2021-01-15 22:02:54 -0500

    creating the file structure. Server-side Back-End

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,41 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+  type User {
+    _id: ID
+    username: String
+    email: String
+    bookCount: Int
+    savedBooks: [Book]
+  } /* ready . delete when the time comes */
+
+  type Book {
+    bookId: String
+    authors: [String]
+    description: String
+    title: String
+    image: String
+    link: String
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  type Query {
+    me: User
+    users: [User]
+    user(username: String!): User
+    books: [Book]
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    savedBook(title: String!, authors: [String]!, description: String!, bookId: String!, image: String!, link: String!): User
+    removeBook(bookId: String!): User
+  }
+`;
+
+module.exports = typeDefs;
